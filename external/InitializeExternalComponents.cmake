05a420c50608c3823671044501201927947673cc
舒媛 <shu.yuan@synyi.com>
2022-06-29 19:55:50 +0800

    update

--- external/InitializeExternalComponents.cmake
+++ external/InitializeExternalComponents.cmake
@@ -7,19 +7,14 @@ endif()
 include(FetchContent)
 
 
-    FetchContent_Declare(zlib
-        GIT_REPOSITORY https://github.com/madler/zlib.git
-        GIT_TAG v1.2.12)
+FetchContent_Declare(zlib
+    GIT_REPOSITORY https://github.com/madler/zlib.git
+    GIT_TAG v1.2.12)
 
-    FetchContent_GetProperties(zlib)
-    if(NOT zlib_POPULATED)
-        FetchContent_Populate(zlib)
-        add_subdirectory(${zlib_SOURCE_DIR} ${zlib_BINARY_DIR} EXCLUDE_FROM_ALL)
-    endif()
-
-    include_directories(
-        "${zlib_SOURCE_DIR}"
-        "${zlib_BINARY_DIR}")
-    set(ZLIB_LIBRARIES zlibstatic CACHE STRING "ZLIB_LIBRARIES" FORCE)
+FetchContent_GetProperties(zlib)
+if(NOT zlib_POPULATED)
+    FetchContent_Populate(zlib)
+    add_subdirectory(${zlib_SOURCE_DIR} ${zlib_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
 
     
